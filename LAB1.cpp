132a269ad83475888985ef74a4cadf9bf5200198
fionamartynova <fiona.victorovna@gmail.com>
2026-09-13 07:00:01 +0300

    Лаба 1: оракул чисел (генерация массива, подсчет чётных/нечётных, сумма 3 и 5) с doc-строками и массивом как параметром в фунции

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-bool isEven(int num);
-bool isDivisible(int num, int divider);
+int countEven(int *array, const int N);
+int sumDivisible(int *array, int const N, int divider);
+
 int main(){
+
     srand(time(0));
     const int N = 20;
     int array[N];
     int even_count = 0;
     int summ_divided_three = 0;
     int summ_divided_five = 0;
+
     for(int i = 0; i < N; i++){
         array[i] = rand() % 1000 + 1;
-            //std::cout << array[i] << std::endl;
     }
-    for(int i = 0; i < N; i++){
-            even_count = even_count + isEven(array[i]);
-        }
-        std::cout << "Chetnye: " << even_count << ", Nechetnye: " << N - even_count << std::endl;
-    for(int i = 0; i < N; i++){
-        if(isDivisible(array[i], 3)){
-            summ_divided_three = summ_divided_three + array[i];
-        }
-        if(isDivisible(array[i], 5)){
-            summ_divided_five = summ_divided_five + array[i];
-        }
-    }
+    
+    even_count = countEven(array, N);
+    std::cout << "Chetnye: " << even_count << ", Nechetnye: " << N - even_count << std::endl;
+    
+    summ_divided_three = sumDivisible(array, N, 3);
+    summ_divided_five = sumDivisible(array, N, 5);
+        
+    
     std::cout << "Summa chisel kratnyh 3: " << summ_divided_three << std::endl;
     std::cout << "Summa chisel kratnyh 5: " << summ_divided_five << std::endl;
     return 0;
 }
+
 /*
- * Проверка числа на чётность
+ * Подсчёт количества чётных чисел в массиве
  *
- * @param num проверяемое целое число.
- * @return true, если число чётное, иначе false.
+ * @param array указатель на массив.
+ * @param N количество элементов в массиве.
+ * @return количество чётных чисел.
  */
-bool isEven(int num){
-    if(num % 2 == 0){
-        return 1;
-    }else{
-        return 0;
+int countEven(int *array, const int N){
+    int count = 0;
+    for(int i = 0; i < N; i++){
+        if(array[i] % 2 == 0){
+            count++;
+        }
     }
+    return count;
 }
 /*
- * Проверка делимости числа на заданный делитель
+ * Суммирование чисел, кратных заданному делителю
  *
- * @param num проверяемое целое число.
- * @param divider делитель, на который проверяется число.
- * @return true, если число делится на делитель без остатка, иначе false.
+ * @param array указатель на массив.
+ * @param N количество элементов в массиве.
+ * @param divider делитель.
+ * @return сумма чисел, делящихся на делитель без остатка.
  */
-bool isDivisible(int num, int divider){
-    if(num % divider == 0){
-        return true;
-    }else{
-        return false;
+int sumDivisible(int *array, int const N, int divider){
+    int summa = 0;
+    for(int i = 0; i < N; i++){
+        if(array[i] % divider == 0){
+            summa += array[i];
+        }
     }
+    return summa;
 }
